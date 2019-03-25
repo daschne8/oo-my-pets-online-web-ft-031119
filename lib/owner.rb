6c07071be16abc67195fc69d9bafa8c0c960c0f1
Daniel Schneider <daschne8@gmail.com>
2019-03-25 18:09:05 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,11 @@
 class Owner
-  # code goes here
+  attr_accessor
+  attr_reader
+  @@all = []
+  def initialize
+    @pets = {fishes: [], cats: [], dogs: []}
+  end
+  def self.reset_all
+    @@all = []
+  end
 end

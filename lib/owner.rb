ec0736d4bf056b55ddb34322807c06c81f92c0e2
Daniel Schneider <daschne8@gmail.com>
2019-03-25 18:12:43 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,11 +1,14 @@
 class Owner
-  attr_accessor
+  attr_accessor :name
   attr_reader :species
   @@all = []
   def initialize
     @pets = {fishes: [], cats: [], dogs: []}
     @species = "human"
     @@all << self
+  end
+  def sey_species
+    puts "I am #{@species}."
   end
   def self.reset_all
     @@all = []

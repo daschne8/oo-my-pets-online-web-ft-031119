af3b0753a0bc95416426a19cb8fb61f772bf8d7a
Daniel Schneider <daschne8@gmail.com>
2019-03-25 18:38:06 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -37,5 +37,9 @@ class Owner
   def play_with_cats
     self.pets[:cats].each { |cat| cat.mood = "happy"  }
   end
+  def feed_fish
+    self.pets[:fishes].each {|fish| fish.mood = "happy"}
+  end
+
 
 end
